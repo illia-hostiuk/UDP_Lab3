1752bce0b56998d49fdab4e9abbb50d3d1f35527
illia-hostiuk <hostiuk.illia@chnu.edu.uagit config --listgit config --global user.name illia-hostiukgit config --global user.email hostiuk.illia@chnu.edu.ua>
2026-05-26 18:27:23 +0300

    Initial UDP Lab 3

---
+++ UDPClient/UDPClient.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <fstream>
+#include <vector>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+vector<char> readFile(const char* fileName)
+{
+    ifstream file(fileName, ios::binary);
+
+    return vector<char>(
+        (istreambuf_iterator<char>(file)),
+        istreambuf_iterator<char>()
+    );
+}
+
+void sendEnd(SOCKET sock, sockaddr_in& serverAddr)
+{
+    sendto(
+        sock,
+        "END",
+        3,
+        0,
+        (sockaddr*)&serverAddr,
+        sizeof(serverAddr)
+    );
+}
+
+int main()
+{
+    WSADATA wsaData;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        cout << "WSAStartup failed\n";
+
+        return 1;
+    }
+
+    cout << "WinSock initialized\n";
+
+    vector<char> fileData = readFile("test.tex");
+
+    cout << "File loaded successfully\n";
+
+    SOCKET clientSocket = socket(
+        AF_INET,
+        SOCK_DGRAM,
+        IPPROTO_UDP
+    );
+
+    if (clientSocket == INVALID_SOCKET)
+    {
+        cout << "Socket creation failed\n";
+
+        WSACleanup();
+
+        return 1;
+    }
+
+    cout << "UDP socket created\n";
+
+    sockaddr_in serverAddr;
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(644);
+
+    inet_pton(
+        AF_INET,
+        "127.0.0.1",
+        &serverAddr.sin_addr
+    );
+
+    // WHOLE FILE
+
+    cout << "\n=============================\n";
+    cout << "Sending whole file...\n";
+
+    sendto(
+        clientSocket,
+        fileData.data(),
+        fileData.size(),
+        0,
+        (sockaddr*)&serverAddr,
+        sizeof(serverAddr)
+    );
+
+    sendEnd(clientSocket, serverAddr);
+
+    cout << "Whole file sent successfully\n";
+
+    // 44 FRAGMENTS
+
+    cout << "\n=============================\n";
+    cout << "Sending file as 44 fragments...\n";
+
+    int totalSize = fileData.size();
+
+    int fragmentSize = totalSize / 44;
+
+    int offset = 0;
+
+    for (int i = 0; i < 44; i++)
+    {
+        int currentSize;
+
+        if (i == 43)
+            currentSize = totalSize - offset;
+        else
+            currentSize = fragmentSize;
+
+        sendto(
+            clientSocket,
+            &fileData[offset],
+            currentSize,
+            0,
+            (sockaddr*)&serverAddr,
+            sizeof(serverAddr)
+        );
+
+        cout << "Fragment "
+            << i + 1
+            << " sent. Size: "
+            << currentSize
+            << endl;
+
+        offset += currentSize;
+    }
+
+    sendEnd(clientSocket, serverAddr);
+
+    cout << "\n44 fragments sent successfully\n";
+
+    closesocket(clientSocket);
+
+    WSACleanup();
+
+    cout << "\nUDP Client closed\n";
+
+    return 0;
+}
---
+++ UDPServer/UDPServer.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <fstream>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void receiveFile(SOCKET serverSocket, const char* outputFile)
+{
+    sockaddr_in clientAddr;
+    int clientSize = sizeof(clientAddr);
+
+    char buffer[1024];
+
+    ofstream file(outputFile, ios::binary);
+
+    cout << "Receiving file...\n";
+
+    while (true)
+    {
+        int bytesReceived = recvfrom(
+            serverSocket,
+            buffer,
+            sizeof(buffer),
+            0,
+            (sockaddr*)&clientAddr,
+            &clientSize
+        );
+
+        if (bytesReceived <= 0)
+            break;
+
+        // END marker
+        if (bytesReceived == 3 &&
+            buffer[0] == 'E' &&
+            buffer[1] == 'N' &&
+            buffer[2] == 'D')
+        {
+            break;
+        }
+
+        file.write(buffer, bytesReceived);
+
+        cout << "Received block size: "
+            << bytesReceived << endl;
+    }
+
+    file.close();
+
+    cout << "File saved as: "
+        << outputFile << endl;
+}
+
+int main()
+{
+    WSADATA wsaData;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        cout << "WSAStartup failed\n";
+        return 1;
+    }
+
+    cout << "WinSock initialized\n";
+
+    SOCKET serverSocket = socket(
+        AF_INET,
+        SOCK_DGRAM,
+        IPPROTO_UDP
+    );
+
+    if (serverSocket == INVALID_SOCKET)
+    {
+        cout << "Socket creation failed\n";
+
+        WSACleanup();
+
+        return 1;
+    }
+
+    cout << "UDP socket created\n";
+
+    sockaddr_in serverAddr;
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(644);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(
+        serverSocket,
+        (sockaddr*)&serverAddr,
+        sizeof(serverAddr)
+    ) == SOCKET_ERROR)
+    {
+        cout << "Bind failed. Error: "
+            << WSAGetLastError() << endl;
+
+        closesocket(serverSocket);
+
+        WSACleanup();
+
+        return 1;
+    }
+
+    cout << "Bind successful\n";
+
+    cout << "\n=============================\n";
+    cout << "WAITING WHOLE FILE\n";
+
+    receiveFile(
+        serverSocket,
+        "received_whole.tex"
+    );
+
+    cout << "\n=============================\n";
+    cout << "WAITING 44 FRAGMENTS\n";
+
+    receiveFile(
+        serverSocket,
+        "received_fragments.tex"
+    );
+
+    closesocket(serverSocket);
+
+    WSACleanup();
+
+    cout << "\nUDP Server closed\n";
+
+    return 0;
+}
